10b48716133f02cf176c104340419e7337acc8d6
Md Somad <103892160+mdsomad@users.noreply.github.com>
2023-11-05 21:15:44 +0530

    crew box complete

--- src/app/crew/page.jsx
+++ src/app/crew/page.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import "./crew.css";
+import crewData from "@/models/crewData/crewData";
 const crew = () => {
-  return <div>Crew</div>;
+  return (
+    <div className="mainContainer">
+      {crewData.map((data) => {
+        return (
+          <div className="childContainer">
+            <div className="crewTextBox">
+              <h1 className="nameTag">Name : {data.name}</h1>
+              <h1 className="roleTag">Role : {data.role}</h1>
+              <div style={{ display: "flex" }}>
+                <h1 className="websiteLinkTextTag">Website Link :</h1>
+                <h1 className="linkTag"> {data.websiteLink}</h1>
+              </div>
+            </div>
+            <div style={{ display: "flex", flexGrow: "1" }}></div>
+            <a target="_blank" href={data.websiteLink}>
+              <button className="visitButton">Visit Website</button>
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default crew;
---
+++ src/models/crewData/crewData.jsx
@@ -0,0 +1,9 @@
+const crewData = [
+  {
+    name: "Artaza Sameen",
+    role: "Web Developer",
+    websiteLink: "https://artaza.in",
+  },
+];
+
+export default crewData;
