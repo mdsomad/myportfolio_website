fb260d10080fcca02be2383607d1f930b2b8aa09
Md Somad <103892160+mdsomad@users.noreply.github.com>
2023-11-17 17:52:20 +0530

    liveapps change

--- src/app/projects/liveapps/page.jsx
+++ src/app/projects/liveapps/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import "./liveApps.css";
+import liveAppsCss from "./liveapps.module.css";
 import LiveAppBox from "@/Component/ProjectBox/LiveAppBox";
 import ProjectsnavigationBar from "@/Component/ProjectsNavigationBar/ProjectsnavigationBar";
 import React from "react";
@@ -9,7 +9,7 @@ const liveApps = () => {
   return (
     <>
       <ProjectsnavigationBar />
-      <div className="VewContainer">
+      <div className={liveAppsCss.VewContainer}>
         {liveAppData.map((data,index) => (
           <LiveAppBox key={index} liveAppData={data} />
         ))}
